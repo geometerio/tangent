5d5a8b773478c4651d04cb8a218233ec064c8d0f
Eric Saxby <sax@geometer.io>
2021-07-08 13:51:12 -0700

    Remove hello world

--- lib/tangent.ex
+++ lib/tangent.ex
@@ -2,17 +2,4 @@ defmodule Tangent do
   @moduledoc """
   Documentation for `Tangent`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tangent.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/tangent_test.exs
+++ test/tangent_test.exs
@@ -1,8 +1,4 @@
 defmodule TangentTest do
   use ExUnit.Case
   doctest Tangent
-
-  test "greets the world" do
-    assert Tangent.hello() == :world
-  end
 end
